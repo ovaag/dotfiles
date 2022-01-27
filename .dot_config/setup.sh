4ad22759e8b5f5f4ee241890da54e676bd9984c9
Ola Chr. Vaage <ola.christoffer.vage@scoutdi.com>
2022-01-27 12:56:10 +0100

    update zshrc and p10k setup

--- .dot_config/setup.sh
+++ .dot_config/setup.sh
@@ -10,16 +10,15 @@ fc-cache -f -v
 
 # Install zsh stuff
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s "$(command -v zsh)"
+chsh -s "$(which zsh)"
 ZSH_CUSTOM=~/.oh-my-zsh/custom
 git clone -q https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
-echo "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" >>~/.zshrc
 
 # Clone dotfile repo
 git clone --bare git@github.com:ovaag/dotfiles.git $HOME/.dotfiles
-alias dots='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dots="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 dots checkout --force
 dots config --local status.showUntrackedFiles no
 dots fetch
